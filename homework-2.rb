26e63b9daf9ef06312b8cc78c802dfd91ff1690f
camzillajs101 <camilomason0@gmail.com>
2017-03-19 12:35:47 -0400

    Deleting tests

--- homework-2.rb
+++ homework-2.rb
@@ -6,6 +6,3 @@ require 'google_drive'
 
 session = GoogleDrive::Session.from_config("homework-2_config.json")
 ws = session.spreadsheet_by_key("13zeA2CQagu4xLwTuFrBoQSozv5jOY16xifq2EDHNf3s").worksheets[0]
-ws[1,2] = nil
-ws.save();
-puts ws[1,2] # Should return nothing
