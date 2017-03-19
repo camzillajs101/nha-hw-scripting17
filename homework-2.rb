3ca7bc194fa872bbaa81844df4e491d57015818a
camzillajs101 <camilomason0@gmail.com>
2017-03-19 10:04:33 -0400

    Some commits I do not know of.

--- homework-2.rb
+++ homework-2.rb
@@ -3,3 +3,9 @@
 =end
 
 require 'google_drive'
+
+session = GoogleDrive::Session.from_config("homework-2_config.json")
+ws = session.spreadsheet_by_key("13zeA2CQagu4xLwTuFrBoQSozv5jOY16xifq2EDHNf3s").worksheets[0]
+ws[1,2] = ""
+ws.save();
+puts ws[1,2] # Should return nothing
