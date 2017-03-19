3b7b0e447561e393f97d217760aa4a3ddd315772
camzillajs101 <camilomason0@gmail.com>
2017-03-19 08:36:46 -0400

    Deleting more

--- homework-1.rb
+++ homework-1.rb
@@ -7,4 +7,18 @@ File.open("homework-1_numbers.csv") do |file|
 	end
 end
 
-puts array
+def format(ary)
+	final = []
+	first = []
+	middle = []
+	last = []
+	ary.each do |i|
+		temp = ary[i]
+		first << temp[0..2]
+		middle << temp[3..5]
+		last << temp[6..8]
+		final << "+1 (#{first[i]})-#{middle[i]}-#{last[i]}"
+	end
+	final
+end
+puts format([3476684595])
