7c46c6e9dbef46045b53930eecb4a46fc1497e43
camzillajs101 <camilomason0@gmail.com>
2017-06-26 12:20:48 -0400

    Adding app.rb

---
+++ Homework 4/app.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+
+puts "What do you want to search for?"
+search = gets.chomp
+url = "https://www.google.com/alerts/preview?params=%5Bnull\
+%2C%5Bnull%2Cnull%2Cnull%2C%5Bnull%2C%22#{search}%22%2C%22c\
+om%22%2C%5Bnull%2C%22en%22%2C%22US%22%5D%2Cnull%2Cnull%2Cnu\
+ll%2C0%2C1%5D%2Cnull%2C3%2C%5B%5Bnull%2C1%2C%22user%40examp\
+le.com%22%2C%5Bnull%2Cnull%2C23%5D%2C2%2C%22en-US%22%2Cnull\
+%2Cnull%2Cnull%2Cnull%2Cnull%2C%220%22%2Cnull%2Cnull%2C%22A\
+B2Xq4hcilCERh73EFWJVHXx-io2lhh1EhC8UD8%22%5D%5D%5D%2C0%5D"
+puts url
+doc = Nokogiri::HTML(open(url))
+doc.css(".result_set").each do |x|
+  puts x.content
+end
