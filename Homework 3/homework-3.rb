51090b6bf8c83bea4f20e80eaf3518f5830249ee
camzillajs101 <camilomason0@gmail.com>
2017-06-29 09:31:44 -0400

    Adding new variables

--- Homework 3/homework-3.rb
+++ Homework 3/homework-3.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'json'
 require 'open-uri'
 
+hash = {}
+iterations = 0
 url = "https://www.twitter.com"
 doc = Nokogiri::HTML(open(url))
 doc.css("span.username.u-dir b").each do |x|
